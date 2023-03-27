a39f08fd950705961e958375b9643a9bd12a7116
Artem <2002maslovartem@gmail.com>
2023-03-27 16:24:29 +0500

    add functionallity

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,120 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+interface Param {
+  id: number;
+  name: string;
+  type: 'text';
 }
 
-export default App
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+
+interface Model {
+  paramValues: ParamValue[];
+  colors: Color[];
+}
+
+interface Props {
+  params: Param[];
+  model: Model;
+  onChange: (model: Model) => void;
+}
+
+interface Color {
+  id: number;
+  color: string;
+}
+
+const TextParamEditor = ({
+  param,
+  value,
+  onChange,
+}: {
+  param: Param;
+  value: string;
+  onChange: (value: string) => void;
+}) => {
+  return (
+    <div>
+      <label>{param.name}:</label>
+      <input
+        type={param.type}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  );
+};
+
+const ModelEditor = ({ params, model, onChange }: Props) => {
+  const [paramValues, setParamValues] = useState(model.paramValues);
+
+  const handleParamChange = (paramId: number, value: string) => {
+    const newParamValues = paramValues.map((item) =>
+      item.paramId === paramId ? { ...item, value } : item
+    );
+    setParamValues(newParamValues);
+    onChange({ ...model, paramValues: newParamValues });
+  };
+
+  return (
+    <div>
+      {params.map((param) => {
+        const paramValue = paramValues.find((p) => p.paramId === param.id);
+        const value = paramValue ? paramValue.value : '';
+        return (
+          <TextParamEditor
+            key={param.id}
+            param={param}
+            value={value}
+            onChange={(value) => handleParamChange(param.id, value)}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default function App() {
+  const params: Param[] = [
+    {
+      id: 1,
+      name: 'Назначение',
+      type: `text`,
+    },
+    {
+      id: 2,
+      name: 'Длина',
+      type: `text`,
+    },
+  ];
+
+  const model: Model = {
+    paramValues: [
+      {
+        paramId: 1,
+        value: 'повседневное',
+      },
+      {
+        paramId: 2,
+        value: 'макси',
+      },
+    ],
+    colors: [
+      { id: 1, color: 'черный' },
+      { id: 2, color: 'белый' },
+    ],
+  };
+
+  const handleModelChange = (newModel: Model) => {
+    console.log(newModel);
+  };
+
+  return (
+    <div>
+      <ModelEditor params={params} model={model} onChange={handleModelChange} />
+    </div>
+  );
+}
